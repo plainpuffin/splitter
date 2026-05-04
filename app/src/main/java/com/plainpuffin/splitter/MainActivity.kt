017312d2f4529bdb4f66c34b3d2b177606726829
plainpuffin <plainpuffin@gmail.com>
2026-05-04 20:12:34 +0000

    Fix Splitter button contrast build error

--- app/src/main/java/com/plainpuffin/splitter/MainActivity.kt
+++ app/src/main/java/com/plainpuffin/splitter/MainActivity.kt
@@ -45,6 +45,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
