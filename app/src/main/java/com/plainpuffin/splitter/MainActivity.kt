a191bee96ffb992a2df7fc0dc3b6fe145cf571c3
plainpuffin <plainpuffin@gmail.com>
2026-05-05 20:11:34 +0000

    Fix missing launcher icon import

--- app/src/main/java/com/plainpuffin/splitter/MainActivity.kt
+++ app/src/main/java/com/plainpuffin/splitter/MainActivity.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
