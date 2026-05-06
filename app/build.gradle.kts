054f2460ee533d95da7c45e88904002baccc1b1a
plainpuffin <plainpuffin@gmail.com>
2026-05-06 06:10:02 +0000

    Remove unused Compose runtime/preview deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,13 +62,10 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
 
